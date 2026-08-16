7c9401458cae8f9b59f6375d6702ddb7e171dd19
ci4r439 <testusergrawal@gmail.com>
2026-08-16 13:14:33 +0530

    find_repeats itt1

--- scan/scan.cu
+++ scan/scan.cu
@@ -197,6 +197,8 @@ __global__ void iseq(int * input, int * output, int N){
     }
     if(input[x]==input[x+1]){
         output[x]=1;
+    }else{
+        output[x]=0;
     }
     return;
 }
@@ -216,12 +218,12 @@ int find_repeats(int* device_input, int length, int* device_output) {
 
     int threads_per_block=512;
 
-    iseq<<<(N/threads_per_block)==0?1: (N/threads_per_block),threads_per_block>>>(device_input,device_output,length);
+    iseq<<<(N%threads_per_block)!=0?(N/threads_per_block)+1: (N/threads_per_block),threads_per_block>>>(device_input,device_output,length);
     exclusive_scan(device_output, length);
     cudaDeviceSynchronize();
     int * num=(int*)malloc(sizeof(int));
     cudaMemcpy(num,device_output +(length-1), sizeof(int), cudaMemcpyDeviceToHost);
-    isneq<<<(N/threads_per_block)==0?1: (N/threads_per_block),threads_per_block>>>(device_output,device_input,N);
+    isneq<<<(N%threads_per_block)!=0?(N/threads_per_block)+1: (N/threads_per_block),threads_per_block>>>(device_output,device_input,N);
 
     cudaDeviceSynchronize();
 
