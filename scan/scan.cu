8cc411204be1d171fe599c639865e6acacdc5b10
ci4r439 <testusergrawal@gmail.com>
2026-08-16 12:24:06 +0530

    find_repeats itt1

--- scan/scan.cu
+++ scan/scan.cu
@@ -216,10 +216,12 @@ int find_repeats(int* device_input, int length, int* device_output) {
     int threads_per_block=512;
     std::cout<<"\n\n\nHELLO"<<std::endl;
     iseq<<<N/threads_per_block,threads_per_block>>>(device_input,device_output,length);
+    cudaDeviceSynchronize();
     std::cout<<"\n\n\nHELLO1"<<std::endl;
     exclusive_scan(device_output, length);
-    std::cout<<"\n\n\nHELLO2"<<std::endl;
+    
     cudaDeviceSynchronize();
+    std::cout<<"\n\n\nHELLO2"<<std::endl;
     int num = device_output[N-1];
     std::cout<<"\n\n\nthe number is "<<num<<std::endl;
     isneq<<<N/threads_per_block,threads_per_block>>>(device_output,device_input,N);
