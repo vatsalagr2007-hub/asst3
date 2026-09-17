cb9d5792a2c6a89c1bd3e2701623ed874579b38f
ci4r439 <testusergrawal@gmail.com>
2026-09-17 21:51:46 +0530

    abc

--- scan/scan.cu
+++ scan/scan.cu
@@ -225,7 +225,7 @@ int find_repeats(int* device_input, int length, int* device_output) {
     isneq<<<length%threads_per_block==0?length/threads_per_block:length/threads_per_block+1,threads_per_block>>>(Value,length,device_output);
     cudaDeviceSynchronize();
     int *x;
-    cudaMemcpy(Value[N-1], x, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(Value+N-1, x, sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(Value);
     return * x;
 
