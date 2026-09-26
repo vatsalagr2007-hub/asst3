787be9fa1795d998f70ee38cb3d8c3f4ae57036c
ci4r439 <testusergrawal@gmail.com>
2026-09-26 16:12:34 +0530

    test

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -820,12 +820,12 @@ CudaRenderer::render() {
 
     kernelRenderCircles<<<gridDim, blockDim>>>();
     cudaDeviceSynchronize();
-    //     dim3 blockDim1(16, 16, 1);
-    // dim3 gridDim1(
-    //     (image->width + blockDim.x - 1) / blockDim.x,
-    //     (image->height + blockDim.y - 1) / blockDim.y);
+    dim3 blockDim1(16, 16, 1);
+    dim3 gridDim1(
+        (image->width + blockDim.x - 1) / blockDim.x,
+        (image->height + blockDim.y - 1) / blockDim.y);
 
-    //shadePixels<<<gridDim1,blockDim1>>>();
+    shadePixels<<<gridDim1,blockDim1>>>();
     cudaDeviceSynchronize();
 
 }
